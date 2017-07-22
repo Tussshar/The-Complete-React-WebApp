bd0e0ad600c7982379b3e2ba0825d4cc61aa9b02
Tussshar <tusharkale52@gmail.com>
2017-07-22 18:52:53 -0400

    Section3, Lecture 10: React Component
    Displaying Simple web page using React Component

--- Hello React/public/app.jsx
+++ Hello React/public/app.jsx
@@ -1,4 +1,18 @@
+var Greeter = React.createClass({
+  render: function(){//This function expects that u renter some jsx code that can be rendered in browser
+
+    //When you are going to return JSX, you have to return only one root element
+    return (
+      <div>
+        <h1>Hello React!!</h1>
+        <p>This is displayed using component</p>
+      </div>
+      //Adding second div right here wont work. Everything has to be in single element only
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <Greeter/>,
   document.getElementById('app')
 );
