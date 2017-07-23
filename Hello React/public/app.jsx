623482ac6a231c879a95d290da0b65ffc0452e0f
Tussshar <tusharkale52@gmail.com>
2017-07-23 09:32:49 -0400

    Sec 3, Lect 12: Component Properties
    Displaying simple web page by passing data into component

--- Hello React/public/app.jsx
+++ Hello React/public/app.jsx
@@ -1,18 +1,35 @@
 var Greeter = React.createClass({
+
+//Incase we didnt pass values to the component then default values will be use.
+  getDefaultProps: function(){
+    return {
+        name: 'Kale',
+        message: 'This is the default message'
+    };
+  },
   render: function(){//This function expects that u renter some jsx code that can be rendered in browser
+
+    /*
+      We can access props using this.props object
+      this.props has all the object
+    */
+    var name = this.props.name;
+    var message = this.props.message;
 
     //When you are going to return JSX, you have to return only one root element
     return (
       <div>
-        <h1>Hello React!!</h1>
-        <p>This is displayed using component</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
       </div>
       //Adding second div right here wont work. Everything has to be in single element only
     );
   }
 });
 
+var firstName = 'Tushar';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message="Message from prop"/>,
   document.getElementById('app')
 );
