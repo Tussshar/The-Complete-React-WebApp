8fc5fd13780116a06cf130b4d81971ecaef432ce
Tussshar <tusharkale52@gmail.com>
2017-07-23 10:08:18 -0400

    Sec3, Lect 13 & 14: User Events, Callbacks and Component State
    Taking input from user and using state to display that value

--- Hello React/public/app.jsx
+++ Hello React/public/app.jsx
@@ -7,13 +7,34 @@ var Greeter = React.createClass({
         message: 'This is the default message'
     };
   },
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e){
+    /*
+      This will stop form from submitting and cause the page to refresh
+    */
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';//This would make input box empty
+
+    if(typeof name === 'string' && name.length > 0){
+      this.setState({
+        name: name
+      });
+    }
+  },
   render: function(){//This function expects that u renter some jsx code that can be rendered in browser
 
     /*
       We can access props using this.props object
       this.props has all the object
     */
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message;
 
     //When you are going to return JSX, you have to return only one root element
@@ -21,6 +42,11 @@ var Greeter = React.createClass({
       <div>
         <h1>Hello {name}!</h1>
         <p>{message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
       //Adding second div right here wont work. Everything has to be in single element only
     );
