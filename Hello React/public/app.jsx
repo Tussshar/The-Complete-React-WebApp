b57239764073ad5621de82cf929e649ecc97cac2
Tussshar <tusharkale52@gmail.com>
2017-07-23 12:18:01 -0400

    Sec3, Lect15: Nested Component
    static version of nested component

--- Hello React/public/app.jsx
+++ Hello React/public/app.jsx
@@ -1,3 +1,24 @@
+var GreeterMessage = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Some H1</h1>
+        <p>Some paragraph</p>
+      </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  render: function(){
+    return (
+      <form>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
 var Greeter = React.createClass({
 
 //Incase we didnt pass values to the component then default values will be use.
@@ -43,10 +64,9 @@ var Greeter = React.createClass({
         <h1>Hello {name}!</h1>
         <p>{message + '!!'}</p>
 
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name"/>
-          <button>Set Name</button>
-        </form>
+        <GreeterMessage/>
+        <GreeterForm/>
+
       </div>
       //Adding second div right here wont work. Everything has to be in single element only
     );
